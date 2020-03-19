75e14bd59f92c721409323eeaaee187271eb662d
HenryRivera <henry.rivera@nyu.edu>
2020-03-18 20:02:50 -0400

    final commit

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
+import sys
+
+
 def mapping(s1, s2):
     vals = {}
     # Checking to see if lengths are the same
     if len(s1) != len(s2):
-        print("false")
-        return
+        return False
     # If lengths are the same
     for i in s1:
         if i not in vals:
             vals[i] = 1
         else:
             # If duplicates in s1 return False
-            print("false")
-            return
+            return False
     # End of loops, no duplicates found in s1, return True
-    print("true")
+    return True
+
+
+def main(s1, s2):
+    res = mapping(s1, s2)
+    if res:
+        print("true")
+    else:
+        print("false")
+
+
+main(sys.argv[1], sys.argv[2])
